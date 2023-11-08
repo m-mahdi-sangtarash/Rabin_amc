2a5195d745fde817a04e1b38cb5e43d446671811
Mohammad Mahdi Sangtarash <m.mahdi.sangtarash@gmail.com>
2023-11-08 22:37:48 +0330

    Text menu - Continue(class)

--- text_main.py
+++ text_main.py
@@ -1,40 +1,41 @@
 import proj_func as pf
 
 
-class Selection_Screen():
+class Selection_Screen:
+    year_lst = pf.year_list()
+    if not year_lst:
+        print('\n1.Create a new year'
+              '\n2.Quit')
+        task = int(input('Please choose: '))
+        if task == 1:
+            year_num = int(input('Enter year number: '))
+            pf.create_year(year_num)
+        elif task == 2:
+            exit()
 
+    else:
+        print('\n1.Choose from list'
+              '\n2.Create a new year'
+              '\n3.Quit')
+        task = int(input('Please choose: '))
+        if task == 1:
+            on = 0
+            for year in year_lst:
+                print(f'{on}.{year}')
+                on += 1
+            task = int(input('\nChoose year: '))
 
+        elif task == 2:
+            year_num = int(input('\nEnter year number: '))
+            pf.create_year(year_num)
 
+        elif task == 3:
+            exit()
 
-year_lst = pf.year_list()
-if not year_lst:
-    print('\n1.Create a new year'
-          '\n2.Quit')
-    task = int(input('Please choose: '))
-    if task == 1:
-        year_num = int(input('Enter year number: '))
-        pf.create_year(year_num)
-    elif task == 2:
-        exit()
-
-else:
-    print('\n1.Choose from list'
-          '\n2.Create a new year'
-          '\n3.Quit')
-    task = int(input('Please choose: '))
-    if task == 1:
-        on = 0
-        for year in year_lst:
-            print(f'{on}.{year}')
-            on += 1
-        task = int(input('\nChoose year: '))
-
-    elif task == 2:
-        year_num = int(input('\nEnter year number: '))
-        pf.create_year(year_num)
-
-    elif task == 3:
-        exit()
+class Add_income:
+    amount = int(input('Amount: '))
+    category = int(input('\nCategory?'
+                         '\n1.'))
 
 print('1.Add a income'
       '\n2.Add a cost'
