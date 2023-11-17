14b910d150ba902a4b010d0ae49b34f64aa0967e
Mohammad Mahdi Sangtarash <m.mahdi.sangtarash@gmail.com>
2023-11-17 15:42:44 +0330

    edit add_cost function

--- proj_func.py
+++ proj_func.py
@@ -271,9 +271,9 @@ def percent_year():
 def add_income(amount, category_num, year_num, month_num):
     dataframe = pd.read_excel(f'year_data/year-{year_num}.xlsx',
                               sheet_name=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', 'total'])
-
+    sheet = dataframe[f'{month_num}']
+    sheet.iloc[category_num, sheet.columns.get_loc('incomes')] = amount
 
 def add_cost(amount, category_num, year_num, month_num):
     dataframe = pd.read_excel(f'year_data/year-{year_num}.xlsx',
                               sheet_name=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', 'total'])
-    dataframe[month_num]['income'][category_num] = amount
